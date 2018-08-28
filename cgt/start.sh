07f6945d87d9b07ac1b705354d22aa5ce950918b
lamp.zl <zl03jsj@163.com>
2018-08-28 10:37:30 +0800

    spell error

--- cgt/start.sh
+++ cgt/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./geth --datadir ./daata --rpc -rpcapi 'db,net,admin,miner,eth,web3,debug,personal' --rpccorsdomain "*" --rpcport 8100 --ws --wsaddr=127.0.0.1 --wsport 8500 --wsapi "db,personal,eth,net,web3,debug" --wsorigins "*"  console
+./geth --datadir ./data --rpc -rpcapi 'db,net,admin,miner,eth,web3,debug,personal' --rpccorsdomain "*" --syncmode "full" --rpcport 8100 --ws --wsaddr=127.0.0.1 --wsport 8500 --wsapi "db,personal,eth,net,web3,debug" --wsorigins "*"  console
 
